c68a64eb1e83c3085bff191ee736ea5d252aefa7
vaclav.benes.1 <vaclav.benes.1@gmail.com>
2021-01-12 18:17:31 +0100

    positon of last quote in class

--- src/cursor.ts
+++ src/cursor.ts
@@ -1,5 +1,29 @@
 import { TextLine } from 'vscode'
 
+
+
+const positionBeforeSecondQuote = (letters: string): number | null => {
+    let count = 0
+    let index = 0
+
+
+    for (const letter of letters) {
+        if (letter === "\"") {
+            count += 1
+        }
+
+        if (count === 2) {
+            return index
+        }
+
+        index += 1
+    }
+
+    return null
+}
+
+
+
 export function cursorPositionBeforeQuote(line: TextLine): number | null {
     let count = 0
     let index = 0
@@ -36,3 +60,21 @@ export function cursorPositionBeforeCloseTag(line: TextLine): number | null {
     }
     return null
 }
+
+export function cursorPositionInClassTag(line: TextLine): number | null {
+
+    const tag = "class="
+
+
+    if (!line.text.includes(tag)) return null
+
+    const tagStartIndex = line.text.indexOf(tag) + tag.length
+
+    const dataAfterClass = line.text.slice(tagStartIndex, line.text.length)
+
+    const pos = positionBeforeSecondQuote(dataAfterClass) || 0
+
+    return tagStartIndex + pos
+}
+
+
